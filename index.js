4815bc6003c0c921743f187c5d7863c2505bcebe
Patch-o <francisco.javier.climent@bootcamp-upgrade.com>
2023-03-14 01:42:48 +0100

    cors added/ readme.md

--- index.js
+++ index.js
@@ -1,37 +1,54 @@
-const express = require('express');
-const passport = require('passport');
-require('./src/utils/authentication/index');
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
-const usersRoutes =  require('./src/api/users/users.routes')
-const db = require('./src/utils/database/database');
-// const dotenv = require('dotenv');
+const express = require("express");
+const passport = require("passport");
+require("./src/utils/authentication/index");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const cors = require("cors");
+const usersRoutes = require("./src/api/users/users.routes");
+const db = require("./src/utils/database/database");
+const dotenv = require("dotenv");
+dotenv.config();
 db.connectDb();
 const app = express();
 const PORT = process.env.PORT || 6000;
 
-
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Methods", "POST, GET, PUT, PATCH, DELETE");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
 
 //la sesion tiene que ir antes de inicializar passpor y despues de express.json
-app.use(session({
+app.use(
+  session({
     secret: process.env.SESSION_SECRET,
     saveUninitialized: false,
     resave: false,
     cookie: {
-        maxAge: 2 *60*60* 1000
+      maxAge: 2 * 60 * 60 * 1000,
     },
-    store: MongoStore.create({mongoUrl: db.DB_URL})
+    store: MongoStore.create({ mongoUrl: db.DB_URL }),
     //con esta linea ⤴️, dp de instalar connect-mongo, se crea y gestiona automatic'
     // una coleccion llamada sesion que guarda las sesiones de los users en la BBDD
-}))
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
 // app.use('/', usersRoutes);
-app.use('/users', usersRoutes)
+app.use("/users", usersRoutes);
 
 app.listen(PORT, () => {
-    console.log(`running like BOLT in http://localhost:${PORT}`);
-})
+  console.log(`running like BOLT in http://localhost:${PORT}`);
+});
